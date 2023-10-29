1a7a364a724d7dbf52999361c8189a57b598ec56
D8thisland <digital-tech@8isles.com>
2023-10-29 11:14:03 +0200

    added rate limiter

--- MULTI-Y/worker.js
+++ MULTI-Y/worker.js
@@ -2,7 +2,23 @@ addEventListener("fetch", (event) => {
     event.respondWith(handleRequest(event.request));
 });
 
+// Define rate limit settings
+const rateLimit = {
+    windowMs: 60000, // 1 minute window
+    maxRequests: 100, // Max 100 requests per minute
+};
+
+const rateLimiter = createRateLimiter(rateLimit);
+
 async function handleRequest(request) {
+    // Use the rate limiter to check if the request exceeds the rate limit
+    const rateLimitExceeded = await rateLimiter.check(request);
+
+    if (rateLimitExceeded) {
+        return new Response("Rate limit exceeded", { status: 429 });
+    }
+
+    // Your existing code here
     const url = new URL(request.url);
     const path = url.pathname;
     const domain = url.hostname.replace(/^www\./, '');  // Remove 'www.' prefix if present
@@ -22,7 +38,7 @@ async function handleRequest(request) {
     const transformedResponse = new HTMLRewriter()
         .on("head", new HeadHandler(domain))
         .transform(originalResponse);
-    
+
     return transformedResponse;
 }
 
@@ -59,7 +75,7 @@ class HeadHandler {
                     iframeContainer.style.position = 'fixed';
                     iframeContainer.style.top = '0';
                     iframeContainer.style.left = '0';
-                    
+
                     const iframe = document.createElement('iframe');
                     iframe.src = "${iframeURL}";
                     iframe.style.width = '100%';
@@ -90,4 +106,36 @@ class HeadHandler {
 
         element.append(preloadLink + viewportMetaTag + styles + jsInjection, { html: true });
     }
+}
+
+// Helper function to create a rate limiter
+function createRateLimiter(options) {
+    const rateLimiter = {
+        key: (request) => request.headers.get("CF-Connecting-IP"), // Use IP address as the rate-limiting key
+        rateLimit: new Map(),
+        options: options,
+        async check(request) {
+            const key = this.key(request);
+            const now = Date.now();
+            const rateInfo = this.rateLimit.get(key) || { count: 0, resetTime: now + options.windowMs };
+
+            if (rateInfo.resetTime < now) {
+                // Reset the counter if the window has passed
+                rateInfo.count = 0;
+                rateInfo.resetTime = now + options.windowMs;
+            }
+
+            if (rateInfo.count >= options.maxRequests) {
+                // Exceeded rate limit
+                return true;
+            }
+
+            // Increment the request count
+            rateInfo.count++;
+            this.rateLimit.set(key, rateInfo);
+            return false;
+        },
+    };
+
+    return rateLimiter;
 }
